c0cb8301c3261661123f4014eca439f6e3ddb828
youichi_io <60864155+youichi-fukumoto@users.noreply.github.com>
2021-07-21 14:34:16 +0900

    テキストボックスの値を取得し、初期化する

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  alert();
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  alert(inputText);
 };
 
 document
