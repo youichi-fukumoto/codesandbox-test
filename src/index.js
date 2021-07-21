ead21e641513bdf5b548631dab4dfd53b48294fc
youichi_io <60864155+youichi-fukumoto@users.noreply.github.com>
2021-07-21 14:53:14 +0900

    完了ボタンと削除ボタンの追加

--- src/index.js
+++ src/index.js
@@ -8,17 +8,31 @@ const onClickAdd = () => {
   // divタグ生成
   const div = document.createElement("div");
   div.className = "list-row";
-  console.log(div);
 
   // liタグ生成
   const li = document.createElement("li");
   li.innerText = inputText;
-  console.log(li);
+
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
 
   //divタグの小要素の各要素を設定
   div.appendChild(li);
-  console.log(div);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
+  //未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
 };
 
