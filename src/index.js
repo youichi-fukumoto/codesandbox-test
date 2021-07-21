36dff7fdabc5fcb81d91006249e0f3699d22c826
youichi_io <60864155+youichi-fukumoto@users.noreply.github.com>
2021-07-21 14:45:54 +0900

    未完了リストに追加

--- src/index.js
+++ src/index.js
@@ -4,7 +4,22 @@ const onClickAdd = () => {
   // テキストボックスの値を取得し、初期化する
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+
+  // divタグ生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  console.log(div);
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  console.log(li);
+
+  //divタグの小要素の各要素を設定
+  div.appendChild(li);
+  console.log(div);
+
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
